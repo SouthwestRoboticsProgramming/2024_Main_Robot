53b059e95f6bfaed680ca88d5f45de7cd589b8e9
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-09-22 08:22:29 -0500

    [ShuffleLog] Store persistence as JSON

--- ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/Tool.java
+++ ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/Tool.java
@@ -1,5 +1,11 @@
 package com.swrobotics.shufflelog.tool;
 
+import com.google.gson.JsonObject;
+import com.swrobotics.shufflelog.json.JsonObj;
+
 public interface Tool {
     void process();
+
+    default void load(JsonObj obj) {}
+    default void store(JsonObject obj) {}
 }
