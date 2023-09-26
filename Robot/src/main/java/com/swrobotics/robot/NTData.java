c0495ffe23f88fb69811c5ace715efaed7ad4433
Mason Vogt <mvog2501@mpsedu.org>
2023-09-25 22:03:15 -0500

    Revert "[Robot] Deleted NTData"
    This reverts commit 7b07d772b6f9621765d24949b60c41921bfc34d9.

---
+++ Robot/src/main/java/com/swrobotics/robot/NTData.java
@@ -0,0 +1,11 @@
+package com.swrobotics.robot;
+
+/**
+ * Class to store all tunable NetworkTables values in one place, to make it easier to hardcode the
+ * defaults in case of data loss.
+ *
+ * <p>Temporary values should not be here, this class is only for persistent data.
+ */
+public final class NTData {
+    // TODO: Put NetworkTables data here
+}
