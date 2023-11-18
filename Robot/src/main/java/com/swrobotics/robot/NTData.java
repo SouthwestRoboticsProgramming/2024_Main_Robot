7f1543ea60d4a72c8a7aa0da65b5cdd940a9fb21
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-11-18 07:46:15 -0600

    Add NT offsets

--- Robot/src/main/java/com/swrobotics/robot/NTData.java
+++ Robot/src/main/java/com/swrobotics/robot/NTData.java
@@ -1,5 +1,8 @@
 package com.swrobotics.robot;
 
+import com.swrobotics.lib.net.NTDouble;
+import com.swrobotics.lib.net.NTEntry;
+
 /**
  * Class to store all tunable NetworkTables values in one place, to make it easier to hardcode the
  * defaults in case of data loss.
@@ -7,5 +10,8 @@ package com.swrobotics.robot;
  * <p>Temporary values should not be here, this class is only for persistent data.
  */
 public final class NTData {
-    // TODO: Put NetworkTables data here
+    public static final NTEntry<Double> FL_OFFSET = new NTDouble("Swerve/Modules/Front Left Offset", 0).setPersistent();
+    public static final NTEntry<Double> FR_OFFSET = new NTDouble("Swerve/Modules/Front Right Offset", 0).setPersistent();
+    public static final NTEntry<Double> BL_OFFSET = new NTDouble("Swerve/Modules/Back Left Offset", 0).setPersistent();
+    public static final NTEntry<Double> BR_OFFSET = new NTDouble("Swerve/Modules/Back Right Offset", 0).setPersistent();
 }
--- Robot/src/main/java/com/swrobotics/robot/subsystems/swerve/SwerveModule.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/swerve/SwerveModule.java
@@ -2,6 +2,7 @@ package com.swrobotics.robot.subsystems.swerve;
 
 import com.swrobotics.lib.net.NTDouble;
 import com.swrobotics.lib.net.NTEntry;
+import com.swrobotics.robot.config.CANAllocation;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
@@ -12,10 +13,10 @@ public interface SwerveModule {
             int driveId, int turnId, int encoderId,
             Translation2d position,
             NTEntry<Double> offset) {
-        public Info(int driveId, int turnId, int encoderId, double x, double y, String name) {
-            this(driveId, turnId, encoderId,
+        public Info(CANAllocation.SwerveIDs ids, double x, double y, NTEntry<Double> offset) {
+            this(ids.drive, ids.turn, ids.encoder,
                     new Translation2d(x, y),
-                    new NTDouble("Swerve/Modules/" + name + " Offset", 0).setPersistent());
+                    offset);
         }
     }
 
