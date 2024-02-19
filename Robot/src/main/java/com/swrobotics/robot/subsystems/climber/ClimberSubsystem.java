a11b64ad5d0851b831eebaae6d8462d9b676f824
rmheuer <63077980+rmheuer@users.noreply.github.com>
2024-02-18 22:23:05 -0600

    [Robot] Add LED indicator for climber hold

--- Robot/src/main/java/com/swrobotics/robot/subsystems/climber/ClimberSubsystem.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/climber/ClimberSubsystem.java
@@ -10,9 +10,12 @@ public final class ClimberSubsystem extends SubsystemBase {
     private final ClimberArm leftArm = new ClimberArm(IOAllocation.CAN.CLIMBER_L_MOTOR, InvertedValue.Clockwise_Positive);
     private final ClimberArm rightArm = new ClimberArm(IOAllocation.CAN.CLIMBER_R_MOTOR, InvertedValue.CounterClockwise_Positive);
 
+    private ClimberArm.State currentState;
+
     public void setState(ClimberArm.State state) {
         leftArm.setState(state);
         rightArm.setState(state);
+        currentState = state;
     }
 
     @Override
@@ -31,5 +34,9 @@ public final class ClimberSubsystem extends SubsystemBase {
 
     public TalonFX getRightMotor() {
         return rightArm.getMotor();
+    }
+
+    public ClimberArm.State getCurrentState() {
+        return currentState;
     }
 }
